db58d7d7f9693b93f47d4e19ec592e03b8858c40
carlosfpj <cafer89@gmail.com>
2023-11-22 21:41:22 -0500

    add nested route to designerpage

--- frontend/src/pages/Designerpage.tsx
+++ frontend/src/pages/Designerpage.tsx
@@ -1,9 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Button } from "react-bootstrap";
+import { Link, NavLink, Outlet } from "react-router-dom";
 
 const DesignerPage = () => {
   return (
     <>
       <h1>DesignerPage</h1>
+      <Link to="/designer/line">
+        <Button>Lineas</Button>
+      </Link>
       <Outlet/>
     </>
   )
