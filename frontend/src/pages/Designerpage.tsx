38309bdf1964804cbdb51ab068706cf4e6862dd5
carlosfpj <cafer89@gmail.com>
2023-11-24 22:55:06 -0500

    add styles to aside

--- frontend/src/pages/Designerpage.tsx
+++ frontend/src/pages/Designerpage.tsx
@@ -1,28 +1,51 @@
-import { Button } from "react-bootstrap";
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { Button, Accordion } from "react-bootstrap";
+import { Link, Outlet } from "react-router-dom";
 import styles from "../styles/Designerpage.module.css";
 
 const DesignerPage = () => {
   return (
     <body>
-      <div className={styles.layout}>
+      <div className={styles.layoutRow}>
         <aside className={styles.aside}>
-          <Link to="/designer/line">
-            <Button>Lineas</Button>
-          </Link>
-          <Link to="/designer/separator">
-            <Button>Separadores</Button>
-          </Link>
-          <Link to="/designer/scrubber">
-            <Button>Scrubbers</Button>
-          </Link>
-          <Link to="/designer/kod">
-            <Button>KO-Drum</Button>
-          </Link>
+          <Accordion flush>
+            <Accordion.Item eventKey="0">
+              <Accordion.Header>Líneas</Accordion.Header>
+              <Accordion.Body className={styles.layoutColumn}>
+                <Link to="/designer/line">
+                  <Button>Una Fase</Button>
+                </Link>
+                <Link to="/designer/line">
+                  <Button>Bifásica</Button>
+                </Link>
+                <Link to="/designer/line">
+                  <Button>Multifásica</Button>
+                </Link>
+              </Accordion.Body>
+            </Accordion.Item>
+            <Accordion.Item eventKey="1">
+              <Accordion.Header>Separadores</Accordion.Header>
+              <Accordion.Body className={styles.layoutColumn}>
+                <Link to="/designer/separator">
+                  <Button>Bifásicos</Button>
+                </Link>
+                <Link to="/designer/separator">
+                  <Button>Trifásicos</Button>
+                </Link>
+              </Accordion.Body>
+            </Accordion.Item>
+            <div className={styles.layoutColumn}>
+              <Link to="/designer/scrubber">
+              <button>Scrubber</button>
+              </Link>
+              <Link to="/designer/kod">
+              <button>KO-Drum</button>
+              </Link>
+            </div>
+          </Accordion>
         </aside>
         <main>
+          <Outlet />
         </main>
-        <Outlet/>
       </div>
     </body>
 
