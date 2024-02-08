8808ac03b185e8094c2705583577f18ccfe632d4
carlosfpj <cafer89@gmail.com>
2024-02-08 06:06:53 -0500

    adding Min and Max roughness coeff for every material, still 16 to go

--- server/src/util/roughness.ts
+++ server/src/util/roughness.ts
@@ -2,19 +2,185 @@ const pipeRoughnessConstants = [
   {
     id: 1,
     prop: {
-            material: "Drawn Cooper, Lead, Brass, new Aluminum",
-            coeff: 0.00000328,
-          }
+      material: "Drawn Cooper, Lead, Brass, new Aluminum",
+      feet : {
+        MinCoeff: 0.00000328,
+        MaxCoeff: 0.00000656,
+      }
+    }
   },
   {
     id: 2,
     prop: {
-            material: "PVC, PE and other smooth Plastic Pipes",
-            coeff: 0.0000049,
+      material: "PVC, PE and other smooth Plastic Pipes",
+      feet: {
+        MinCoeff: 0.0000049,
+        MaxCoeff: 0.0000230,
+      }
     }
-  }
+  },
+  {
+    id: 3,
+    prop: {
+      material: "Stainless steel, bead blasted",
+      feet: {
+        coeff: 0.00000328,
+      }
+    }
+  },
+  {
+    id: 4,
+    prop: {
+      material: "Stainless steel, turned",
+      feet: {
+        coeff: 0.00000131,
+      }
+    }
+  },
+  {
+    id: 5,
+    prop: {
+      material: "Stainless steel, electron-polished",
+      feet: {
+        coeff: 0.000000328,
+      }
+    }
+  },
+  {
+    id: 6,
+    prop: {
+      material: "Commercial steel or wrought iron",
+      feet: {
+        coeff: 0.000148,
+      }
+    }
+  },
+  {
+    id: 7,
+    prop: {
+      material: "Stretched steel",
+      feet: {
+        coeff: 0.0000495,
+      }
+    }
+  },
+  {
+    id: 8,
+    prop: {
+      material: "Weld steel",
+      feet: {
+        coeff: 0.000148,
+      }
+    }
+  },
+  {
+    id: 9,
+    prop: {
+      material: "Galvanized steel",
+      feet: {
+        coeff: 0.000492,
+      }
+    }
+  },
+  {
+    id: 10,
+    prop: {
+      material: "Rusted steel (corrosion)",
+      feet: {
+        coeff: 0.000492,
+      }
+    }
+  },
+  {
+    id: 11,
+    prop: {
+      material: "New cast iron",
+      feet: {
+        coeff: 0.00082,
+      }
+    }
+  },
+  {
+    id: 12,
+    prop: {
+      material: "Worn cast iron",
+      feet: {
+        coeff: 0.00262,
+      }
+    }
+  },
+  {
+    id: 13,
+    prop: {
+      material: "Rusty cast iron",
+      feet: {
+        coeff: 0.00492,
+      }
+    }
+  },
+  {
+    id: 14,
+    prop: {
+      material: "Sheet or asphalted cast iron",
+      feet: {
+        coeff: 0.0000328,
+      }
+    }
+  },
+  {
+    id: 15,
+    prop: {
+      material: "Smoothed cement",
+      feet: {
+        coeff: 0.00098,
+      }
+    }
+  },
+  {
+    id: 16,
+    prop: {
+      material: "Ordinary concrete",
+      feet: {
+        coeff: 0.00098,
+      }
+    }
+  },
+  {
+    id: 17,
+    prop: {
+      material: "Coarse concrete",
+      feet: {
+        coeff: 0.00098,
+      }
+    }
+  },
+  {
+    id: 18,
+    prop: {
+      material: "Wood stove",
+      feet: {
+        coeff: 0.00059,
+      }
+    }
+  },
+  {
+    id: 19,
+    prop: {
+      material: "Well planed wood",
+      feet: {
+        coeff: 0.00059,
+      }
+    }
+  },
+  {
+    id: 20,
+    prop: {
+      material: "Ordinary wood",
+      feet: {
+        coeff: 0.00164,
+      }
+    }
+  },
 ];
 
-
 export default pipeRoughnessConstants;
-
