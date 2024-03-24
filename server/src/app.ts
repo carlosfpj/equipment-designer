f397d9879f739ce8dd799d5686d3a5f24efad473
carlosfpj <cafer89@gmail.com>
2024-03-24 16:59:45 -0500

    to production, here we go again... x33

--- server/src/app.ts
+++ server/src/app.ts
@@ -10,7 +10,7 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/", homeRouter);
-app.use("/line/singlephase/vel", router);
+app.use("/line/singlephase", router);
 //app.use("https://equipment-designer-api.onrender.com/", router );
 
 app.use((req, res, next) => {
