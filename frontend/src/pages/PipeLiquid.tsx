2ff1e7bbdf96a2b6ca53f1bbebf1b9efdb5f36ce
carlosfpj <cafer89@gmail.com>
2024-03-26 14:43:41 -0500

    to production, here we go again... x49

--- frontend/src/pages/PipeLiquid.tsx
+++ frontend/src/pages/PipeLiquid.tsx
@@ -84,7 +84,7 @@ const PipeLiquid = () => {
           }),
         });
         if(res.status === 200) {
-          const resJson = await res.json(JSON.parse(JSON.stringify(resJson)));
+          const resJson = await res.json();
           // const resText = await res.text();
           console.log(JSON.parse(JSON.stringify(resJson)));
           setResultVelocity(resJson.vel);
