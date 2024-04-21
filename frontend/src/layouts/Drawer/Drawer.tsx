be31d4fb016d160e9f9894779a8941e17afd5b66
carlosfpj <cafer89@gmail.com>
2024-04-20 19:50:19 -0500

    add front drawer x6

--- frontend/src/layouts/Drawer/Drawer.tsx
+++ frontend/src/layouts/Drawer/Drawer.tsx
@@ -17,62 +17,73 @@ const Drawer = () => {
 
   return (
     <nav className={navClass}>
-      <NavLink
-        to="/designer"
-        className="drawerlink"
-        style={activeState}>
-        Designer overview
-      </NavLink>
-      <details className="details">
-        <summary className="summary">Pipes</summary>
-        <div className="buttonslayout">
-          <NavLink
-            to="/designer/pipes/singlephase/liquid"
-            className="drawerlink">
-            Single-phase, liquid
-          </NavLink>
-          <NavLink
-            to="/designer/pipes/singlephase/gas"
-            className="drawerlink">
-            Single-phase, gas.
-          </NavLink>
-          <NavLink
-            to="/designer/pipes/twophase"
-            className="drawerlink">
-            Two-phase.
-          </NavLink>
-          <NavLink
-            to="/designer/pipes/multiphase"
-            className="drawerlink">
-            Multi-phase.
-          </NavLink>
-        </div>
-      </details>
-      <details className="details">
-        <summary className="summary">Equipment</summary>
-        <div className="buttonslayout">
-          <NavLink
-            to="/designer/equipment/separator/twophase"
-            className="drawerlink">
-            Two-phase Separators
-          </NavLink>
-          <NavLink
-            to="/designer/equipment/separator/threephase"
-            className="drawerlink">
-            Three-phase Separators
-          </NavLink>
-          <NavLink
-            to="/designer/equipment/scrubber"
-            className="drawerlink">
-            Scrubber
-          </NavLink>
-          <NavLink
-            to="/designer/equipment/kod"
-            className="drawerlink">
-            KO-Drum
-          </NavLink>
-        </div>
-      </details>
+      <div className="navlayout">
+        <NavLink
+          to="/designer"
+          className="drawerlink"
+          style={activeState}
+          >
+          Designer overview
+        </NavLink>
+        <details className="details">
+          <summary className="summary">Pipes</summary>
+          <div className="buttonslayout">
+            <NavLink
+              to="/designer/pipes/singlephase/liquid"
+              className="drawerlink"
+              style={activeState}>
+              Single-phase, liquid
+            </NavLink>
+            <NavLink
+              to="/designer/pipes/singlephase/gas"
+              className="drawerlink"
+              style={activeState}>
+              Single-phase, gas.
+            </NavLink>
+            <NavLink
+              to="/designer/pipes/twophase"
+              className="drawerlink"
+              style={activeState}>
+              Two-phase.
+            </NavLink>
+            <NavLink
+              to="/designer/pipes/multiphase"
+              className="drawerlink"
+              style={activeState}>
+              Multi-phase.
+            </NavLink>
+          </div>
+        </details>
+        <details className="details">
+          <summary className="summary">Equipment</summary>
+          <div className="buttonslayout">
+            <NavLink
+              to="/designer/equipment/separator/twophase"
+              className="drawerlink"
+              style={activeState}>
+              Two-phase Separators
+            </NavLink>
+            <NavLink
+              to="/designer/equipment/separator/threephase"
+              className="drawerlink"
+              style={activeState}>
+              Three-phase Separators
+            </NavLink>
+            <NavLink
+              to="/designer/equipment/scrubber"
+              className="drawerlink"
+              style={activeState}>
+              Scrubber
+            </NavLink>
+            <NavLink
+              to="/designer/equipment/kod"
+              className="drawerlink"
+              style={activeState}>
+              KO-Drum
+            </NavLink>
+          </div>
+        </details>
+      </div>
     </nav>
   )
 }
