7f90bdc7cc0d51f00149acf7cbefa52c689ab370
carlosfpj <cafer89@gmail.com>
2024-04-27 16:01:47 -0500

    add styles on THeory component

--- frontend/src/components/Theory.tsx
+++ frontend/src/components/Theory.tsx
@@ -1,11 +1,12 @@
 import fig1 from '../assets/images/1.png';
 import fig2 from '../assets/images/2.png';
 import fig3 from '../assets/images/3.png';
+import "./Theory.css";
 
 const Theory = () => {
   return (
-    <>
-      <p>Single-phase liquid lines should be sized primarily on the basis of flow velocity.
+    <div className="theory">
+      <p className="p">Single-phase liquid lines should be sized primarily on the basis of flow velocity.
         For lines transporting liquids in single-phase from one pressure vessel to another by pressure differential,
         the flow velocity should not exceed 15ft/s at maximum flow rates, to minimize flashing ahead of the control valve.
         If practical, flow velocity should not be less than 4ft/s to minimize deposition of sand and other solids.
@@ -13,12 +14,12 @@ const Theory = () => {
         Most of the pressure drop in liquid lines between two pressure vessels will occur in the liquid dump
         valve and/or choke.</p>
 
-      <p>1.	Flow velocities in liquid lines may be read from figure 2.1 or may be calculated using the following
+      <p className="p">1.	Flow velocities in liquid lines may be read from figure 2.1 or may be calculated using the following
         derived equation:</p>
 
-      <p>V&#8321; = 0.012 x Q&#8321; / d&#8321;&#xb2;</p>
+      <p className="p">V&#8321; = 0.012 x Q&#8321; / d&#8321;&#xb2;</p>
 
-      <p>where:
+      <p className="p">where:
         <br />
         V&#8321; = average liquid flow velocity, feet/second.<br />
         Q&#8321; = liquid flow rate, barrels/day.<br />
@@ -27,13 +28,13 @@ const Theory = () => {
 
       <img alt='velocity in liquid lines' src={fig1}></img>
 
-      <p>Pressure drop (psi per 100 feet of flow length) for single phase liquid lines may be read
+      <p className="p">Pressure drop (psi per 100 feet of flow length) for single phase liquid lines may be read
         from figure 2 or may be calculated using the following equiation:
       </p>
 
-      <p>&#8710;P = 0.00115f Q&#8321;&#xb2;S&#8321; / d&#8321;&#8309;</p>
+      <p className="p">&#8710;P = 0.00115f Q&#8321;&#xb2;S&#8321; / d&#8321;&#8309;</p>
       <br />
-      <p>where:
+      <p className="p">where:
         <br />
         &#8710;P = pressure drop, psi/100 feet.<br />
         f = Moody friction factor, dimensionless.<br />
@@ -45,14 +46,14 @@ const Theory = () => {
       <img alt='pressure drop in liquid lines' src={fig2}></img>
       <br />
 
-      <p>The Moody friction factor, f is a function of the Reynolds number and the surface roughness of
+      <p className="p">The Moody friction factor, f is a function of the Reynolds number and the surface roughness of
         the pipe. The modified Moody diagram, figure 3, may be used to determine the friction factor once
         the Reynolds number is known. The Reynolds numbre may be determined by the following equiation:
       </p>
 
-      <p>Re = &rho;<sub>L</sub> d<sub>f</sub> V<sub>L</sub> / &mu;<sub>L</sub> </p>
+      <p className="p">Re = &rho;<sub>L</sub> d<sub>f</sub> V<sub>L</sub> / &mu;<sub>L</sub> </p>
       <br />
-      <p>where:
+      <p className="p">where:
         <br />
         Re = Reynolds number, dimensionless.<br />
         &rho;<sub>L</sub> = liquid density, lb/ft<sup>3</sup><br />
@@ -63,7 +64,7 @@ const Theory = () => {
         <span>" "</span> = centistokes times specific gravity divided by 1488<br />
       </p><br />
       <img alt='Moody friction factor' src={fig3}></img><br />
-    </>
+    </div>
   )
 }
 
