549844f73c4c35669cbab1b2daf25528b4a19587
carlosfpj <cafer89@gmail.com>
2024-05-07 22:44:12 -0500

    modified d3 chart in theory.tsx x4

--- frontend/src/utils/chartDataGenerator.tsx
+++ frontend/src/utils/chartDataGenerator.tsx
@@ -3,14 +3,19 @@ let data: {i:number, v:number}[] = [];
 (function data1() {
 
   let v;
-  let d = 0.614;
+  const iterable = [0.614, 0.957, 1.278, 1.500, 1.939,
+                    2.125, 2.323, 2.900, 3.826, 6.065,
+                    7.981, 10.020, 11.938, 13.250, 15.250, 17.250]
 
-
-  for (let i = 100; i < 100000;) {
-    v = (0.012 * (i)) / (d ** 2);
-    let point: { i:number, v:number } = {i,v} ;
-    data.push(point);
-    i = i + 100;
+  for (let d of iterable) {
+    for(let i = 100; i< 100000;) {
+      v = (0.012 * (i)) / (d ** 2);
+      if(v < 50 || v > 0.5) {
+        let point: { i: number, v: number } = { i, v };
+        data.push(point);
+        i = i + 100;
+      } else break;
+    }
   }
 })();
 
