99510347fba5225c75fc32913c060188e5dd8d44
carlosfpj <cafer89@gmail.com>
2024-05-29 22:38:29 -0500

    add form styles in Pipeliquid.tsx

--- frontend/src/pages/PipeLiquid.tsx
+++ frontend/src/pages/PipeLiquid.tsx
@@ -128,123 +128,127 @@ const PipeLiquid = () => {
   }
 
   return (
-    <div className="maincontent">
-      <div className="maincontent-header">
-        <div className="hero">
-          <div className="full-width">
-            <div className="hero-content">
-              <div className="hero-image">
-                <div className="hero-text">
-                  <h1>Single Phase-Liquid</h1>
-                  <h2>Sizing Criteria</h2>
-                  <h3>API RP 14E</h3>
+    <>
+      <div className="maincontent">
+        <div className="maincontent-header">
+          <div className="hero">
+            <div className="full-width">
+              <div className="hero-content">
+                <div className="hero-image">
+                  <div className="hero-text">
+                    <h1>Single Phase-Liquid</h1>
+                    <h2>Sizing Criteria</h2>
+                    <h3>API RP 14E</h3>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className="maincontent-theory">
-        <Theory />
-        {/* <Button onClick={() => setShowAddLineDialog(true)}>Add new Line Calculation</Button> */}
-      </div>
-      <div className="maincontent-form">
-        <div className="form-intro">
-          <p className='p'>If you want to calculate liquid pipes for the oil&gas industry
-            based on the previous equations, please insert the following data.
-            The results will be displayed at the bottom section of the form.
-          </p>
+        <div className="maincontent-theory">
+          <Theory />
         </div>
-        <div className="form">
-          <form onSubmit={handleSubmit}>
-            <div className="radio-group">
-              <p>Choose the type of calculation you want to perform</p>
-              <div className="radio-item">
-                <input type='radio'
-                  id='vel'
-                  name='a'
-                  value="vel"
-                  checked={option === 0}
-                  onChange={() => handleCalculateOption(0)}
-                />
-                <label className="radio-label" htmlFor="vel">Velocity</label>
+        <div className="maincontent-form">
+          <div className="form-intro">
+            <p className='p'>If you want to calculate liquid pipes for the oil&gas industry
+              based on the previous equations, please insert the following data.
+              The results will be displayed at the bottom section of the form.
+            </p>
+          </div>
+          <div className="form">
+            <form onSubmit={handleSubmit}>
+              <div className="radio-group">
+                <p>Choose the type of calculation you want to perform</p>
+                <div className="radio-item">
+                  <input type='radio'
+                    id='vel'
+                    name='a'
+                    value="vel"
+                    checked={option === 0}
+                    onChange={() => handleCalculateOption(0)}
+                  />
+                  <label className="radio-label" htmlFor="vel">Velocity</label>
+                </div>
+                <div className="radio-item">
+                  <input type='radio'
+                    id='Pdrop'
+                    name='a'
+                    value="Pdrop"
+                    checked={option === 1}
+                    onChange={() => handleCalculateOption(1)}
+                  />
+                  <label className="radio-label" htmlFor="Pdrop">Pressure Drop</label>
+                </div>
+
+                <div className="radio-item">
+                  <input type='radio'
+                    id='VP'
+                    name='a'
+                    value="VP"
+                    checked={option === 2}
+                    onChange={() => handleCalculateOption(2)}
+                  />
+                  <label className="radio-label" htmlFor="VP">Velocity and Pressure Drop</label>
+                </div>
               </div>
-              <div className="radio-item">
-                <input type='radio'
-                  id='Pdrop'
-                  name='a'
-                  value="Pdrop"
-                  checked={option === 1}
-                  onChange={() => handleCalculateOption(1)}
-                />
-                <label className="radio-label" htmlFor="Pdrop">Pressure Drop</label>
+
+              <div className="input-group">
+                <label>Liquid flow rate[barrels/day]</label>
+                <input type='text' onChange={handleFlowChange} value={flowRate} /><br />
+                <label>pipe inside diameter[inches]</label>
+                <input type='text' onChange={handlepipeDiameterChange} value={pipeDiameter} /><br />
+
+                {showOption &&
+                  <div>
+                    <label htmlFor='material'>Pipe material</label><br />
+                    <select name='material' id='material' onChange={handlePipeMaterialChange}>
+                      <option>Select one from this dropdown list</option>
+                      <option value="1">1. Drawn Cooper, Lead, Brass, new Aluminum</option>
+                      <option value="2">2. PVC, PE and other smooth Plastic Pipes</option>
+                      <option value="3">3. Stainless steel, bead blasted</option>
+                      <option value="4">4. Stainless steel, turned</option>
+                      <option value="5">5. Stainless steel, electron-polished</option>
+                      <option value="6">6. Commercial steel or wrought iron</option>
+                      <option value="7">7. Stretched steel</option>
+                      <option value="8">8. Weld steel</option>
+                      <option value="9">9. Galvanized steel</option>
+                      <option value="10">10. Rusted steel (corrosion)</option>
+                      <option value="11">11. New cast iron</option>
+                      <option value="12">12. Worn cast iron</option>
+                      <option value="13">13. Rusty cast iron</option>
+                      <option value="14">14. Sheet or asphalted cast iron</option>
+                      <option value="15">15. Smoothed cement</option>
+                      <option value="16">16. Ordinary concrete</option>
+                      <option value="17">17. Coarse concrete</option>
+                      <option value="18">18. Wood stove</option>
+                      <option value="19">19. Well planed wood</option>
+                      <option value="20">20. Ordinary wood</option>
+                    </select>
+                    <br />
+                    <div className="input-group">
+                      <label>Liquid Specific Gravity [S]</label>
+                      <input type='text' onChange={handleSGChange} value={SG} /><br />
+                      <label>Liquid Density [lb/ft3]</label>
+                      <input type='text' onChange={handleDensityChange} value={liquidDensity} /><br />
+                      <label>Liquid Viscocity [Cp]</label>
+                      <input type='text' onChange={handleViscocityChange} value={liquidViscocity} /><br />
+                    </div>
+                  </div>}
               </div>
+              <button>submit</button>
+            </form>
 
-              <div className="radio-item">
-                <input type='radio'
-                  id='VP'
-                  name='a'
-                  value="VP"
-                  checked={option === 2}
-                  onChange={() => handleCalculateOption(2)}
-                />
-                <label className="radio-label" htmlFor="VP">Velocity and Pressure Drop</label>
-              </div>
-            </div>
-
-            <div className="input-group">
-              <label>Liquid flow rate[barrels/day]</label>
-              <input type='text' onChange={handleFlowChange} value={flowRate} /><br />
-              <label>pipe inside diameter[inches]</label>
-              <input type='text' onChange={handlepipeDiameterChange} value={pipeDiameter} /><br />
-
-              {showOption &&
-                <div>
-                  <label htmlFor='material'>Pipe material</label><br />
-                  <select name='material' id='material' onChange={handlePipeMaterialChange}>
-                    <option>Select one from this dropdown list</option>
-                    <option value="1">1. Drawn Cooper, Lead, Brass, new Aluminum</option>
-                    <option value="2">2. PVC, PE and other smooth Plastic Pipes</option>
-                    <option value="3">3. Stainless steel, bead blasted</option>
-                    <option value="4">4. Stainless steel, turned</option>
-                    <option value="5">5. Stainless steel, electron-polished</option>
-                    <option value="6">6. Commercial steel or wrought iron</option>
-                    <option value="7">7. Stretched steel</option>
-                    <option value="8">8. Weld steel</option>
-                    <option value="9">9. Galvanized steel</option>
-                    <option value="10">10. Rusted steel (corrosion)</option>
-                    <option value="11">11. New cast iron</option>
-                    <option value="12">12. Worn cast iron</option>
-                    <option value="13">13. Rusty cast iron</option>
-                    <option value="14">14. Sheet or asphalted cast iron</option>
-                    <option value="15">15. Smoothed cement</option>
-                    <option value="16">16. Ordinary concrete</option>
-                    <option value="17">17. Coarse concrete</option>
-                    <option value="18">18. Wood stove</option>
-                    <option value="19">19. Well planed wood</option>
-                    <option value="20">20. Ordinary wood</option>
-                  </select>
-                  <br />
-                  <div className="input-group">
-                    <label>Liquid Specific Gravity [S]</label>
-                    <input type='text' onChange={handleSGChange} value={SG} /><br />
-                    <label>Liquid Density [lb/ft3]</label>
-                    <input type='text' onChange={handleDensityChange} value={liquidDensity} /><br />
-                    <label>Liquid Viscocity [Cp]</label>
-                    <input type='text' onChange={handleViscocityChange} value={liquidViscocity} /><br />
-                  </div>
-                </div>}
-            </div>
-            <button>submit</button>
-          </form>
-
-          <span>Results</span>
-          <p>velocity: {resultVelocity} ft/seg</p>
-          <p>pressure Drop: {resultPressureDrop} psi/100feet</p>
-          <p>PipeMaterial: {pipeMaterialID}</p>
+            <span>Results</span>
+            <p>velocity: {resultVelocity} ft/seg</p>
+            <p>pressure Drop: {resultPressureDrop} psi/100feet</p>
+            <p>PipeMaterial: {pipeMaterialID}</p>
+          </div>
         </div>
+        <footer className="footer">
+        </footer>
       </div>
-    </div>
+    </>
+
   )
 }
 
